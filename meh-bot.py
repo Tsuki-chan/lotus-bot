26edc11b3d19be2856ab18adf77619df4e2241a0
Lotus <dikatz1323@gmail.com>
2018-03-19 16:24:52 +0100

    Update meh-bot.py

--- meh-bot.py
+++ meh-bot.py
@@ -1,19 +1,19 @@
 import discord
-from discord.ext.command import Bot
+from discord.ext.commands import Bot
 from discord.ext import commands
-import aysnico
+import asyncio
 import time
 
 Client = discord.Client()
-client = command.Bot(comman_prefix = "~")
+client = commands.Bot(command_prefix = " ")
 
 @client.event
-async def on_ready()
+async def on_ready():
   print ("Ready!")
   
 @client.event
 async def on_message(message):
-  if message.content == "cookie":
+  if message.content == "!cookie":
     await client.send_message(message.channel, ":cookie:")
 
   if message.content.startswith('!ping'):
@@ -23,12 +23,12 @@ async def on_message(message):
   if message.content == "!help":
       await client.send_message(message.channel, """
 __**Commands**__
-
 ```- you need to write all small, sorry
 - you can trigger a command by using:
   cookie
 - the developer @Biscuit#0061 is inexperienced
 - updates comming soon! (hopfully)```""")
+
 	
 	
 client.run("Token")
