b83d6c71e67f808a31efa4d22f02e368c4d2c551
Lotus <dikatz1323@gmail.com>
2018-03-19 16:15:43 +0100

    Update meh-bot.py

--- meh-bot.py
+++ meh-bot.py
@@ -15,9 +15,20 @@ async def on_ready()
 async def on_message(message):
   if message.content == "cookie":
     await client.send_message(message.channel, ":cookie:")
+
   if message.content.startswith('!ping'):
       userID = message.author.id
       await client.send_message(message.channel, "<@%s> Pong!" % (userID))
+	
+  if message.content == "!help":
+      await client.send_message(message.channel, """
+__**Commands**__
+
+```- you need to write all small, sorry
+- you can trigger a command by using:
+  cookie
+- the developer @Biscuit#0061 is inexperienced
+- updates comming soon! (hopfully)```""")
 	
 	
 client.run("Token")
