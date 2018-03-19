7caf73081faf34d4e752a8f108690a36670acd2c
Lotus <dikatz1323@gmail.com>
2018-03-19 22:30:51 +0100

    Update meh-bot.py

--- meh-bot.py
+++ meh-bot.py
@@ -5,7 +5,7 @@ import asyncio
 import time
 
 Client = discord.Client()
-client = commands.Bot(command_prefix = " ")
+client = commands.Bot(command_prefix = "~")
 
 @client.event
 async def on_ready():
@@ -13,15 +13,16 @@ async def on_ready():
   
 @client.event
 async def on_message(message):
-  if message.content == "!cookie":
+  if message.content == "~cookie":
     await client.send_message(message.channel, ":cookie:")
 
-  if message.content.startswith('!ping'):
-      userID = message.author.id
-      await client.send_message(message.channel, "<@%s> Pong!" % (userID))
-	
-  if message.content == "!help":
-      await client.send_message(message.channel, """
+  if message.content.startswith('~ping'):
+    userID = message.author.id
+    await client.delete_message(message)
+    await client.send_message(message.channel, "<@%s> Pong!" % (userID))
+
+  if message.content == "~help":
+    await client.send_message(message.channel, """
 __**Commands**__
 ```- you need to write all small, sorry
 - you can trigger a command by using:
@@ -29,6 +30,13 @@ __**Commands**__
 - the developer @Biscuit#0061 is inexperienced
 - updates comming soon! (hopfully)```""")
 
+  if message.content.lower().startswith("~say"):
+    if message.author.id == "209391652870029312":
+      await client.delete_message(message)
+      args = message.content.split(" ")
+      await client.send_message(message.channel,"%s" % (" ".join(args[1:])))
+    else:
+      await client.send_message(message.channel, "Only the owner can use this command!")
 	
 	
 client.run("Token")
